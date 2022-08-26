24e8bae1dd96811f2bd8b42872f27ed9f464585d
JulienChampagnol <julien.champagnol@geode-solutions.com>
2022-08-26 10:35:12 +0200

    feat(wip) : move state back

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,13 @@
 <template>
-    <!-- <v-app-bar app absolute clipped-left>
-      <v-app-bar-nav-icon @click="controlsDrawer = !controlsDrawer" />
-      <img :src="logo" class="logo" @click="resetCamera()">
-      <span class="text-h6">Geode-solutions viewer</span>
-      <v-progress-linear :active="busy" :indeterminate="busy" absolute bottom />
-    </v-app-bar>   -->
-  <!-- <v-main class="appContent">
-    <div style="position: relative; width: 100%; height: 100%">
+  <v-main class="appContent">
+    <!-- <div style="position: relative; width: 100%; height: 100%">
       <remote-rendering-view :client="client" />
-    </div>
-  </v-main> -->
-  <p>toto</p>
-  
-  
+    </div> -->
+  </v-main>
 </template>
 
 <script>
-// import { mapGetters, mapActions } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 // import RemoteRenderingView from '@/components/RemoteRenderingView.vue'
 
@@ -31,23 +22,23 @@ export default {
     // RemoteRenderingView,
   },
   computed: {
-    // ...mapGetters({
-    //   client: 'WS_CLIENT',
-    //   busy: 'WS_BUSY',
-    //   resolution: 'CONE_RESOLUTION',
-    //   height: 'CONE_HEIGHT'
-    // })
+    ...mapGetters({
+      client: 'WS_CLIENT',
+      busy: 'WS_BUSY',
+      resolution: 'CONE_RESOLUTION',
+      height: 'CONE_HEIGHT'
+    })
   },
   mounted () {
-    // this.ws_connect()
+    this.ws_connect()
   },
   methods: {
-    // ...mapActions([
-    //   'setConeResolution',
-    //   'setConeHeight',
-    //   'resetCamera',
-    //   'ws_connect'
-    // ])
+    ...mapActions([
+      'setConeResolution',
+      'setConeHeight',
+      'resetCamera',
+      'ws_connect'
+    ])
   },
 }
 </script>
