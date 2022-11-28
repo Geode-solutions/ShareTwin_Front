b883f22487ae9e2c363f689a32f99c39ec2de4c9
JulienChampagnol <julien.champagnol@geode-solutions.com>
2022-11-28 14:20:50 +0100

    test

--- components/Launcher.vue
+++ components/Launcher.vue
@@ -1,8 +1,8 @@
 <template>
     <v-container justify="space-around">
-        <v-row rows="auto" align-content="center" align="center">
-            <v-col  v-if="((!captchaValidated) && (this.$config.NODE_ENV === 'production'))" cols="auto" align-self="center" align="center">
-              <v-card elevation="5" cols="auto">
+        <v-row align-content="center" align="center">
+            <v-col  v-if="((!captchaValidated) && (this.$config.NODE_ENV === 'production'))" cols="12" align-self="center" align="center">
+              <v-card elevation="5">
                 <v-card-title class="justify-center">
                   Please confirm that you're not a robot
                 </v-card-title>
