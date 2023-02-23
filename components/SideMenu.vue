00ccc83e56f303d3ac2811482050497b8034fda2
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-02-23 17:20:50 +0100

    fix use of ws_link_store in data_store

--- components/SideMenu.vue
+++ components/SideMenu.vue
@@ -10,8 +10,9 @@
         </div>
       </v-expansion-panel-title>
       <v-expansion-panel-text>
-        <v-row v-for="(item, index) in data_tree" :key="index">
-          <component :is="item.component.component_name" :component_options="item.component.component_options" />
+        <v-row v-for="(item, index) in data_tree.items" :key="index">
+          <component :is="item.component.component_name" :component_options="item.component.component_options"
+            :index="index" />
         </v-row>
         <v-row align-content="center" justify="center">
           <v-col cols="auto">
@@ -28,61 +29,75 @@
 <script setup>
 import { use_cloud_store } from '@/stores/cloud'
 import { use_ws_link_store } from '@/stores/ws_link'
+import { use_data_store } from '@/stores/data'
 import FileSelector from '@/components/FileSelector.vue'
 
 const cloud_store = use_cloud_store()
 const ws_link_store = use_ws_link_store()
+const data_store = use_data_store()
 
 const { is_cloud_running } = storeToRefs(cloud_store)
 
 const data_tree = reactive({
-    items: [{
+  items: [
+    {
       file: [],
-      component_name: shallowRef(FileSelector),
-      component_options: {
-        multiple: false,
-        label: 'Please select a file'
-        , accept: ".obj, .vtp"
-      }}]
-    })
+      component: {
+        component_name: shallowRef(FileSelector),
+        component_options: {
+          multiple: false,
+          label: 'Please select a file'
+          , accept: ".obj, .vtp"
+        }
+      }
+
+    }
+  ]
+})
 
 provide('data_tree', data_tree)
 
 async function upload_file () {
   ws_link_store.$patch({ busy: true })
+  console.log('1')
+  for (let i = 0; i < data_tree.items.length; i++) {
+    console.log('2')
+    let current_item = data_tree.items[i]
+    const reader = new FileReader()
+    reader.onload = async function (event) {
+      console.log('4')
+      const params = new FormData()
+      params.append('file', event.target.result)
+      params.append('filename', data_tree.items[i].file[0].name)
+      params.append('filesize', data_tree.items[i].file[0].size)
+      const route = "convertfile"
+      params.append('object', 'PolygonalSurface3D')
+      params.append('extension', 'vtp')
+      console.log('5')
 
-  for (let i = 0; i < data_tree.length; i++) {
-    if (items[i].file.length) {
-      const reader = new FileReader()
-      reader.onload = async function (event) {
-        const params = new FormData()
-        params.append('file', event.target.result)
-        params.append('filename', items[i].file[0].name)
-        params.append('filesize', items[i].file[0].size)
-        const route = "convertfile"
-        params.append('object', 'PolygonalSurface3D')
-        params.append('extension', 'vtp')
-        if (items[i].file.length) {
-          await api_fetch(`/${convertfile}`, {
-            body: params, method: 'POST', onResponse ({ response }) {
-              load(response._data.newFilename)
-            },
-            onError ({ error }) {
-              console.log(error)
-              ws_link_store.$patch({ busy: false })
-            }
-          })
-        }
+      if (current_item.file.length) {
+        console.log('6')
+        await api_fetch(`/geode/convertfile`, {
+          body: params, method: 'POST', onResponse ({ response }) {
+            load(response._data.newFilename)
+          },
+          onError ({ error }) {
+            console.log(error)
+            ws_link_store.$patch({ busy: false })
+          }
+        })
       }
     }
-    if (items[i].file.length) {
-      reader.readAsDataURL(this.items[i].file[0])
+    if (current_item.file.length) {
+      console.log('3.5')
+      console.log(current_item.file[0])
+      reader.readAsDataURL(current_item.file[0])
     }
   }
 }
 
 async function load (DataFilename) {
-  this.send_filenames({ DataFilename })
+  data_store.send_filenames({ DataFilename })
 }
 
 </script>
--- stores/data.js
+++ stores/data.js
@@ -1,15 +1,14 @@
 import { defineStore } from 'pinia'
+import { use_ws_link_store } from './ws_link'
 
 export const use_data_store = defineStore('data', {
   state: () => ({
     filename: '',
   }),
   actions: {
-    async send_filenames ({ rootState }, filenames) {
-      // return dispatch('WS_SEND_FILENAME', files);
-      console.log(rootState)
-      if (rootState.wslink.client) {
-        rootState.wslink.client
+    async send_filenames (filenames) {
+      if (use_ws_link_store.client) {
+        use_ws_link_store.client
           .getRemote()
           .Cone.send_filenames(filenames)
           .catch(console.error);
