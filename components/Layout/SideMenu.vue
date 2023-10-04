eff7e30c81eabe35d26a8c9b9af06353122ecca3
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-10-04 14:51:02 +0200

    cloud_store.is_running getter

--- components/Layout/SideMenu.vue
+++ components/Layout/SideMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <v-navigation-drawer v-model="display_menu" clipped permanent app fixed disable-resize-watcher width="400">
-    <v-expansion-panels v-if="is_cloud_running" multiple accordion dense>
+    <v-expansion-panels v-if="cloud_store.is_running" multiple accordion dense>
       <v-expansion-panel v-for="(item, object_tree_index) in object_tree" :key="object_tree_index">
         <LeftDatasets :object_tree_index="object_tree_index" />
       </v-expansion-panel>
@@ -12,7 +12,5 @@
 const app_store = use_app_store()
 const cloud_store = use_cloud_store()
 
-const { is_cloud_running } = storeToRefs(cloud_store)
 const { display_menu, object_tree } = storeToRefs(app_store)
-
 </script>
