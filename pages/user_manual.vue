551562b2d03aaa64f1145ed706e81fd1cd3301f4
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-05-31 10:17:09 +0200

    add user manual

---
+++ pages/user_manual.vue
@@ -0,0 +1,7 @@
+<template>
+  <iframe :src="user_manual" width="100%" height="100%" />
+</template>
+
+<script setup>
+  import user_manual from "@/assets/documents/User_Manual_share-twin.pdf"
+</script>
